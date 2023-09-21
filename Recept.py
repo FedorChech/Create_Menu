15cdc17234771fbfbe6de3daf5ca96b1a9c9322c
FedorChech <Fedorchechnevkz@gmail.com>
2023-09-21 20:58:55 +0600

    fix add_product_to_dish

--- Recept.py
+++ Recept.py
@@ -1,22 +1,25 @@
 import tkinter as tk
-from tkinter import messagebox
-
-# from tkinter import ttk
 from SqlAlchemy import Product, Session, Dish, DishProduct
 import ttkbootstrap as ttk
-from PIL import Image, ImageTk, ImageFilter  # Импортируем ImageFilter
+from PIL import Image, ImageTk
+from ttkbootstrap import Style
+from ttkbootstrap.constants import *
+from ttkbootstrap.dialogs.dialogs import Messagebox
 
-# window = tk.Tk()
-window = ttk.Window(themename='journal')
-window.title("Apps")
-window.geometry("510x670+500+50")
+window = ttk.Window(themename='superhero')
+window.title("Кулинарный справочник")
+window.geometry("600x700+500+50")
 window.resizable(False, False)
 
-# Устанавливаем стиль для окна с помощью ttkbootstrap
-style = ttk.Style()
-window.configure(background="#EEEEEE")  # Замените "your_color_here" на желаемый цвет фона
-text_color = "#005453"
-fon_color = "#EEEEEE"
+frame_add_prod = ttk.Labelframe(window, text='Добавление продуктов', style='info.TLabelframe', padding=5)
+frame_add_prod.place(x=10, y=10)
+
+frame_add_dish = ttk.Labelframe(window, text='Добавление блюд', style='info.TLabelframe', padding=5)
+frame_add_dish.place(x=300, y=10)
+
+style = Style()
+button_style = (OUTLINE, WARNING)
+
 session = Session()
 
 # Открываем изображение
@@ -35,32 +38,40 @@ tk_image = ImageTk.PhotoImage(resized_image)
 def on_entry_click(event, entry_widget, placeholder_text):
     if entry_widget.get() == placeholder_text:
         entry_widget.delete(0, "end")  # Удалите начальный текст при клике
+        entry_widget.configure(foreground='white')  # Восстановите обычный стиль при клике
 
 
 def on_focusout(event, entry_widget, placeholder_text):
     if entry_widget.get() == '':
         entry_widget.insert(0, placeholder_text)
+        entry_widget.configure(foreground='gray')  # Установите стиль для тусклого текста
 
 
 # Function
 def add_product():
-    product_name = entry_product.get()
-    new_product = Product(ProductName=product_name)
-    session.add(new_product)
-    session.commit()
-    print(f"Продукт {product_name} добавлен")
-    update_product_listbox()
+    if entry_product.get() == "введите продукт":
+        Messagebox.show_info(message="Введите продукт", title="Введите продукт")
+    else:
+        product_name = entry_product.get()
+        new_product = Product(ProductName=product_name)
+        session.add(new_product)
+        session.commit()
+        print(f"Продукт {product_name} добавлен")
+        update_product_listbox()
 
 
 def add_dish():
-    dish_name = entry_dish.get()
-    new_dish = Dish(DishName=dish_name)
-    session.add(new_dish)
-    session.commit()
-    print(f"Блюдо {dish_name} добавлен")
-    update_dish_listbox()
-    display_products_for_dish()
-    update_dish_combobox()
+    if entry_dish.get() == "введите блюдо":
+        Messagebox.show_info(message="Введите блюдо", title="Введите блюдо")
+    else:
+        dish_name = entry_dish.get()
+        new_dish = Dish(DishName=dish_name)
+        session.add(new_dish)
+        session.commit()
+        print(f"Блюдо {dish_name} добавлен")
+        update_dish_listbox()
+        display_products_for_dish()
+        update_dish_combobox()
 
 
 def delete_product():
@@ -111,8 +122,9 @@ def delete_dish():
 
 
 def ask_question_delete_dish():
-    result = messagebox.askquestion("Подтверждение", "Удалить?")
-    if result == "yes":
+    result = Messagebox.show_question("Удалить", buttons=['Нет: primary', 'Да: secondary'], alert=True,
+                                      parent=frame_add_dish)
+    if result == "Да":
         # Выбран вариант "Да"
         print("Вы выбрали 'Да'")
         delete_dish()
@@ -123,13 +135,12 @@ def ask_question_delete_dish():
 
 
 def ask_question_delete_product():
-    result = messagebox.askquestion("Подтверждение", "Удалить?")
-    if result == "yes":
-        # Выбран вариант "Да"
+    result = Messagebox.show_question("Удалить", buttons=['Нет: primary', 'Да: secondary'], alert=True,
+                                      parent=frame_add_dish)
+    if result == "Да":
         print("Вы выбрали 'Да'")
         delete_product()
     else:
-        # Выбран вариант "Нет"
         print("Вы выбрали 'Нет'")
         return
 
@@ -166,29 +177,52 @@ def update_dish_combobox():
     all_dishes = session.query(Dish).all()
 
     # Извлеките только имена блюд и обновите значения в dish_combobox
-    dish_names = [dish.DishName for dish in all_dishes]
-    dish_combobox['values'] = tuple(dish_names)
+    dish_name = [dish.DishName for dish in all_dishes]
+    dish_combobox['values'] = tuple(dish_name)
 
 
-def on_dish_select():
-    selected_dish = dish_combobox.get()
+# def on_dish_select():
+#     # selected_dish = dish_combobox.get()
+#     selected_dish = dish_listbox.curselection()
+#     # Очистите текущее содержимое листбокса
+#     product_listbox.delete(0, tk.END)
+#
+#     # Получите блюдо из базы данных
+#     dish = session.query(Dish).filter_by(DishName=selected_dish).first()
+#
+#     if dish:
+#         # Получите продукты, связанные с выбранным блюдом
+#         products = session.query(Product).join(DishProduct).filter_by(dish_id=dish.id).all()
+#
+#         # Добавьте каждый продукт в листбокс
+#         for product in products:
+#             product_listbox.insert(tk.END, product.ProductName)
 
-    # Очистите текущее содержимое листбокса
-    product_listbox.delete(0, tk.END)
 
-    # Получите блюдо из базы данных
-    dish = session.query(Dish).filter_by(DishName=selected_dish).first()
+def on_dish_select(event=None):
+    selected_index = dish_listbox.curselection()
 
-    if dish:
-        # Получите продукты, связанные с выбранным блюдом
-        products = session.query(Product).join(DishProduct).filter_by(dish_id=dish.id).all()
+    if selected_index:
+        selected_item = dish_listbox.get(selected_index[0])  # Получаем выбранный элемент
 
-        # Добавьте каждый продукт в листбокс
-        for product in products:
-            product_listbox.insert(tk.END, product.ProductName)
+        # Очищаем текущее содержимое Treeview
+        tree.delete(*tree.get_children())  # Удаляем все строки из Treeview
+
+        # Получаем блюдо из базы данных по имени
+        dish = session.query(Dish).filter_by(DishName=selected_item).first()
+
+        if dish:
+            # Получаем записи DishProduct, связанные с выбранным блюдом
+            dish_products = session.query(DishProduct).filter_by(dish_id=dish.id).all()
+
+            # Добавляем каждую запись в Treeview
+            for dish_product in dish_products:
+                product = session.query(Product).filter_by(id=dish_product.product_id).first()
+                tree.insert("", "end", values=(product.ProductName, dish_product.gramm))
 
 
 def display_products_for_dish():
+    # print(dish_listbox.curselection())
     # Очистите текущее содержимое Treeview
     tree.delete(*tree.get_children())  # Удалить все строки из Treeview
 
@@ -211,12 +245,11 @@ def display_products_for_dish():
 def add_products_to_dish():
     selected_dish = dish_combobox.get()
     selected_gramm = entry_gramm.get()
-    if selected_gramm == 'введите граммы':
-        messagebox.showinfo("ПРЕДУПРЕЖДЕНИЕ", "Введите количество грамм продукта!")
-    # Получите блюдо из базы данных
+
     dish = session.query(Dish).filter_by(DishName=selected_dish).first()
 
     if dish:
+
         selected_products = product_listbox.curselection()  # Получите индексы выбранных продуктов
 
         if selected_products:
@@ -290,82 +323,80 @@ def remove_products_from_dish():
 label_image = tk.Label(window, image=tk_image)
 label_image.place(x=310, y=312)
 
-main_label = ttk.Label(window, text="ДОБАВИТЬ ПРОДУКТ", font="arial 12 bold", background=fon_color, bootstyle="danger")
-# main_label.config(foreground=text_color)
-
-main_label.place(x=30, y=10)
-
-entry_product = ttk.Entry(window, width=20)
-entry_product.place(x=10, y=40)
+# _________________FRAME____________________________________
+entry_product = ttk.Entry(frame_add_prod, width=20, foreground='gray')
+# entry_product.place(x=10, y=40)
+entry_product.grid(row=0, column=0)
 entry_product.insert(0, 'введите продукт')
-entry_product.bind("<FocusIn>", lambda event: on_entry_click(event, entry_product, 'введите продукт'))
-entry_product.bind("<FocusOut>", lambda event: on_focusout(event, entry_product, 'введите продукт'))
 
-product_list_label = ttk.Label(window, text="СПИСОК ПРОДУКТОВ", font="arial 9", background=fon_color)
-product_list_label.config(foreground=text_color)
-product_list_label.place(x=40, y=130)
-dish_list_label = ttk.Label(window, text="СПИСОК БЛЮД", font="arial 9", background=fon_color)
-dish_list_label.config(foreground=text_color)
-dish_list_label.place(x=300, y=130)
+add_product_button = ttk.Button(frame_add_prod, text="+", command=add_product, width=5, style=(OUTLINE, WARNING))
+add_product_button.grid(row=0, column=1)
 
-entry_calories = ttk.Entry(window, width=20)
-entry_calories.place(x=10, y=80)
+entry_calories = ttk.Entry(frame_add_prod, width=20, foreground='gray')
+entry_calories.grid(row=1, column=0, pady=10)
 entry_calories.insert(0, 'введите калории')
 entry_calories.bind("<FocusIn>", lambda event: on_entry_click(event, entry_calories, 'введите калории'))
 entry_calories.bind("<FocusOut>", lambda event: on_focusout(event, entry_calories, 'введите калории'))
 
-add_button = ttk.Button(window, text="+", command=add_product, width=6)
-add_button.place(x=143, y=40)
-delete_button = ttk.Button(window, text="УДАЛИТЬ ПРОДУКТ", command=ask_question_delete_product, width=23)
-delete_button.place(x=10, y=268)
-
-product_listbox = tk.Listbox(window, width=25, height=7)
-product_listbox.place(x=10, y=150)
-
-# Widgets Dish
-main_label = ttk.Label(window, text="ДОБАВИТЬ БЛЮДО", font="arial 12 bold", background=fon_color)
-main_label.config(foreground=text_color)
-main_label.place(x=275, y=10)
-
-entry_dish = ttk.Entry(window, width=20)
-entry_dish.place(x=270, y=40)
+entry_dish = ttk.Entry(frame_add_dish, width=20, foreground='gray')
+entry_dish.grid(row=0, column=3)
 entry_dish.insert(0, 'введите блюдо')
 entry_dish.bind("<FocusIn>", lambda event: on_entry_click(event, entry_dish, 'введите блюдо'))
 entry_dish.bind("<FocusOut>", lambda event: on_focusout(event, entry_dish, 'введите блюдо'))
 
-add_dish_button = ttk.Button(window, text="+", command=add_dish, width=5)
-add_dish_button.place(x=400, y=40)
-delete_dish_button = ttk.Button(window, text="УДАЛИТЬ БЛЮДО", command=ask_question_delete_dish, width=23)
-delete_dish_button.place(x=270, y=270)
+add_dish_button = ttk.Button(frame_add_dish, text="+", command=add_dish, width=6, style=(OUTLINE, WARNING))
+add_dish_button.grid(row=0, column=4)
 
-dish_listbox = tk.Listbox(window, width=25, height=7)
-dish_listbox.place(x=270, y=150)
+entry_product.bind("<FocusIn>", lambda event: on_entry_click(event, entry_product, 'введите продукт'))
+entry_product.bind("<FocusOut>", lambda event: on_focusout(event, entry_product, 'введите продукт'))
 
-add_to_dish_button = ttk.Button(window, text="ДОБАВИТЬ", command=add_products_to_dish, width=11)
+product_list_label = ttk.Label(window, text="Список продуктов", font="play 14")
+product_list_label.place(x=45, y=125)
+dish_list_label = ttk.Label(window, text="Список блюд", font="play 14")
+dish_list_label.place(x=320, y=125)
+
+delete_button = ttk.Button(window, text="УДАЛИТЬ ПРОДУКТ", command=ask_question_delete_product, width=27,
+                           style=button_style)
+delete_button.place(x=10, y=273)
+
+product_listbox = tk.Listbox(window, width=30, height=7)
+product_listbox.place(x=10, y=150)
+
+# Widgets Dish
+delete_dish_button = ttk.Button(window, text="УДАЛИТЬ БЛЮДО", command=ask_question_delete_dish, width=27,
+                                style=button_style)
+delete_dish_button.place(x=300, y=275)
+
+dish_listbox = tk.Listbox(window, width=30, height=7)
+dish_listbox.place(x=300, y=150)
+dish_listbox.bind("<<ListboxSelect>>", on_dish_select)
+
+add_to_dish_button = ttk.Button(window, text="ДОБАВИТЬ", command=add_products_to_dish, width=11, style=button_style)
 add_to_dish_button.place(x=10, y=620)
 
-remove_to_dish_button = ttk.Button(window, text="УДАЛИТЬ", command=remove_products_from_dish, width=10)
+remove_to_dish_button = ttk.Button(window, text="УДАЛИТЬ", command=remove_products_from_dish, width=10,
+                                   style=button_style)
 remove_to_dish_button.place(x=135, y=620)
 
-## Запросите блюда из базы данных
+# Запросите блюда из базы данных
 dishes = session.query(Dish).all()
 
 # Создаем список имен блюд
 dish_names = [dish.DishName for dish in dishes]
 
 # Создаем Combobox и устанавливаем список блюд
-dish_combobox = ttk.Combobox(window, values=dish_names, width=29)
+dish_combobox = ttk.Combobox(window, values=dish_names, width=29, foreground='gray')
 dish_combobox.set("Выберите блюдо")
 dish_combobox.place(x=10, y=335)
 
-entry_gramm = ttk.Entry(window, width=31)
+entry_gramm = ttk.Entry(window, width=31, foreground='gray')
 entry_gramm.place(x=10, y=372)
 entry_gramm.insert(0, 'введите граммы')
 entry_gramm.bind("<FocusIn>", lambda event: on_entry_click(event, entry_gramm, 'введите граммы'))
-entry_gramm.bind("<FocusOut>", lambda event: on_focusout(event, entry_gramm, 'введите_граммы'))
+entry_gramm.bind("<FocusOut>", lambda event: on_focusout(event, entry_gramm, 'введите граммы'))
 
 # _______________Tree_____________________________
-tree = ttk.Treeview(window, columns=("Column1", "Column2"))
+tree = ttk.Treeview(window, columns=("Column1", "Column2"), style="SUCCESS")
 
 # Установка заголовков колонок
 tree.heading("#1", text="Название")
@@ -378,12 +409,14 @@ tree.column("#2", width=100, anchor='e')
 
 tree.place(x=10, y=420)
 
-dish_combobox.bind("<<ComboboxSelected>>", on_dish_select)
-dish_combobox.bind("<<ComboboxSelected>>", lambda event=None: display_products_for_dish())
-
 display_products_for_dish()
 
 update_product_listbox()
 update_dish_listbox()
+
+# dish_combobox.bind("<<ComboboxSelected>>", on_dish_select)
+# dish_combobox.bind("<<ComboboxSelected>>", lambda event=None: display_products_for_dish())
+print(dish_listbox.curselection())
+# Привязываем обработчик события к выбору элемента в dish_listbox
 
 window.mainloop()
